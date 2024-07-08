eec250bd1e3ff1d4d3be49d7ae630ec6d466316e
RonaldMcpogger <rameses_amar@dlsu.edu.ph>
2024-07-09 01:40:57 +0800

    made prefab and enemy spawner
    will redo enemy spawner script tomorrow

--- Assets/Scripts/Spawner/EnemySpawner.cs
+++ Assets/Scripts/Spawner/EnemySpawner.cs
@@ -1,20 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [Serializable]
+    public class Pool
+    {
+        public int size;
+        public int index;
+        public GameObject EFab;
+    }
 
     public GameObject Enem;
-     [Range(0,10)] private int EnemyNumbers=0;
-    // Start is called before the first frame update
-    void Awake()
-    {
-       this.Enem = null;
-    }
+    public List<Pool> pools;
+     [Range(0,10)] public int EnemyNumbers=0;
+    public Dictionary<int, Queue<GameObject>> EnemyPool;
+    
      void Start()
     {
-        
+        EnemyPool = new Dictionary<int, Queue<GameObject>>();
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> queue = new Queue<GameObject>();
+
+
+            for(int i = 0; i<pool.size;i++)
+            {
+
+            }
+        }
     }
     // Update is called once per frame
 
