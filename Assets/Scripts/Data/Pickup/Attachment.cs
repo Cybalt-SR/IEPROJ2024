7812162ee5ddc3de146167f7468f419b390a3344
Cybalt-SR <cybalt.sr@gmail.com>
2024-07-16 18:00:59 +0800

    expand map

--- Assets/Scripts/Data/Pickup/Attachment.cs
+++ Assets/Scripts/Data/Pickup/Attachment.cs
@@ -22,13 +22,13 @@ namespace Assets.Scripts.Data.Pickup
         public int damage_added = 0;
         public int shots_per_second_added = 0;
         public int clip_size_added = 0;
-        [Range(0, 1)]public float reload_time_percent_deduction = 0;
+        [Range(-1, 1)]public float reload_time_percent_deduction = 0;
         [Header("Accuracy")]
         public float projectile_speed_added = 0;
-        public float error_angle_percent_deduction = 0;
+        [Range(-1, 1)] public float error_angle_percent_deduction = 0;
         [Header("Spread")]
         public int projectiles_per_shot_added = 0;
-        [Range(0, 1)] public float spread_angle_percent_difference = 0;
+        [Range(-1, 1)] public float spread_angle_percent_difference = 0;
         [Header("Collision")]
         public int bounce_count_added = 0;
         public int split_count_added = 0;
@@ -39,6 +39,8 @@ namespace Assets.Scripts.Data.Pickup
             data.damage += damage_added;
             data.shots_per_second += shots_per_second_added;
             data.clip_size += clip_size_added;
+            data.clip_size = Mathf.Max(1, data.clip_size);
+
             data.reload_time *= 1.0f - reload_time_percent_deduction;
 
             data.projectile_speed += projectile_speed_added;
