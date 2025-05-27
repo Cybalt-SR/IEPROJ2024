3938472da2dcebadc3fac135871087898d85efb8
Enrico Tolentino <80933336+NicoWithSevenIs@users.noreply.github.com>
2025-05-27 11:10:14 +0800

    Secondary
    not tested yet

--- Assets/Scripts/Ability_Optimized/Secondary.cs
+++ Assets/Scripts/Ability_Optimized/Secondary.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -5,22 +6,39 @@ using UnityEngine;
 
 namespace AbilityOP
 {
+    [CreateAssetMenu(fileName = "Secondary", menuName = "Ability Optimized/Secondary", order = 1)]
     public class Secondary : ScriptableObject
     {
         public string secondary_name;
-        [TextArea(1, 3)] public string tool_tip;
+        [TextArea(1, 3)] public string tooltip;
         public Texture2D secondary_icon;
 
-        public string shot_effect_name;
-        public string secondary_ability;
+        public string shot_effect_type;
+        public string secondary_ability_type;
     }
 
     [CustomEditor(typeof(Secondary))]
     public class SecondaryEditor: Editor
     {
+        private void RenderValidator(string effect_type, string type_name)
+        {
+            if (GUILayout.Button($"Validate {effect_type}"))
+            {
+                Type t = Type.GetType(name);
+                Debug.Log(t);
+                bool isTypeValid = t != null && t.IsSubclassOf(typeof(Ability)) && !t.IsAbstract;
+                Debug.LogWarning($"[DEBUG] \"{type_name}\" is {(isTypeValid ? "" : "not")} a valid Ability.");
+            }
+        }
+
         public override void OnInspectorGUI()
         {
+            Secondary secondary = (Secondary)target;
+
             base.OnInspectorGUI();
+            EditorGUILayout.Space(15f);
+            RenderValidator("Shot Effect", secondary.shot_effect_type);
+            RenderValidator("Secondary Ability", secondary.secondary_ability_type);
         }
     }
 
