f60c579dc569394b0e24eb0ed413494973a21e1c
Enrico Tolentino <80933336+NicoWithSevenIs@users.noreply.github.com>
2025-07-15 05:27:53 +0800

    jack in a box update

--- Assets/Scripts/Ability_Optimized/Ability Assets/Circular Gun/Jack_Circular_Gun.cs
+++ Assets/Scripts/Ability_Optimized/Ability Assets/Circular Gun/Jack_Circular_Gun.cs
@@ -6,25 +6,33 @@ using UnityEngine;
 public class Jack_Circular_Gun : MonoBehaviour
 {
 
-    public float detonation_delay = 0.25f;
-    public float detonation_trigger_distance = 3f;
+    public float detonation_delay = 3f;
+    public float detonation_trigger_distance = 5f;
     public float knockback_force = 10f;
     public float explosion_damage = 5;
     public UnitController owner;
 
     private bool ticking_down = false;
     [SerializeField] private List<EnemyController> enemies = new();
-    private float detonation_timer;
+    private float detonation_timer = 0;
+
+    [Header("Visuals")]
+    [SerializeField] private GameObject ticker;
+    [SerializeField] private float tick_interval;
+
+    private float tick_timer = 0;
 
     private void OnEnable()
     {
         ticking_down = false;
         detonation_timer = 0;
-        GetComponent<MeshRenderer>().material.color = Color.black;
+        ticker.SetActive(false);
+        tick_timer = tick_interval;
     }
 
     private void Update()
     {
+
         if (!ticking_down)
         {
             foreach (var e in enemies)
@@ -33,16 +41,24 @@ public class Jack_Circular_Gun : MonoBehaviour
                 if (dist <= detonation_trigger_distance)
                 {
                     ticking_down = true;
-                    GetComponent<MeshRenderer>().material.color = Color.red;
-                    detonation_timer = detonation_delay;
+     
                     break;
                 }
             }
             return;
         }
 
-        
+        Debug.Log("Ticking Down");
+        tick_timer += Time.deltaTime;
+        if (tick_timer > tick_interval * (1.1 - detonation_timer/detonation_delay))
+        {
+            ticker.SetActive(!ticker.activeSelf);
+            tick_timer = 0;
+        }
+
+
         detonation_timer += Time.deltaTime;
+
         if (detonation_timer < detonation_delay)
             return;
 
@@ -58,20 +74,26 @@ public class Jack_Circular_Gun : MonoBehaviour
 
             rb.velocity = Vector3.zero;
             rb.AddForce(knockback_dir * knockback_force, ForceMode.Impulse);
-         
+
+            e.GetComponent<HealthObject>().TakeDamage(explosion_damage, owner);
+
+            /*
             var on_impact = e.gameObject.AddComponent<DoOnImpact>();
             e.enabled = false;
 
             on_impact.exclude_floor = true;
             on_impact.impact_event = () =>
             {
-                e.GetComponent<HealthObject>().TakeDamage(explosion_damage, owner);
+    
                 e.enabled=true;
             };
+            
             on_impact.tag_filter = new() { "Enemy", "Item" };
+            */
         }
 
         gameObject.SetActive(false);
+        Debug.Log("Kaboom");
 
     }
 
